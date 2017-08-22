8c7dc204b9ac82f9ecc80b70212f638e49ac4981
shenfeng1945 <823902826@qq.com>
2017-08-22 23:48:36 +0800

    写完 home.js

--- home.js
+++ home.js
@@ -1,3 +1,10 @@
 /**
  * Created by lzc on 2017/8/22.
  */
+$('ol.tabs').on('click','li',function(){
+    let $li = $(this)
+    let index = $li.index()
+    $li.addClass('active').siblings('.active').removeClass('active')
+    // console.log($('.tabsContent > li').eq(index))
+    $('ol.tabsContent > li').eq(index).addClass('active').siblings('.active').removeClass('active')
+})
