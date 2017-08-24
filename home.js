6f35ea429c7087e5f87dfb75d02374e76c94ea66
shenfeng1945 <823902826@qq.com>
2017-08-24 23:44:41 +0800

    给歌曲列表加上了a标签，相应的要修改css

--- home.js
+++ home.js
@@ -8,3 +8,11 @@ $('ol.tabs').on('click','li',function(){
     // console.log($('.tabsContent > li').eq(index))
     $('ol.tabsContent > li').eq(index).addClass('active').siblings('.active').removeClass('active')
 })
+//假装是AJAX请求
+$(function(){
+    $('.new-lists').css('display','none')
+})
+setTimeout(function(){
+    $('.new-lists').css('display','block')
+    $('.new-music > .loading').css('display','none')
+},800)
