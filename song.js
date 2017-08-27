a20e0ea47e173b9a04831b9eb89d4fa9ac61bac1
shenfeng1945 <823902826@qq.com>
2017-08-27 21:56:02 +0800

    增加歌词滑动效果

--- song.js
+++ song.js
@@ -16,23 +16,66 @@ $('.icon-play').on('click', function () {
     $('.disc-container').addClass('playing')
 })
 
-$(function () {
-    $.get('/song.json').then(function (object) {
-        let { lyric } = object
+// $(function () {
+//     $.get('/song.json').then(function (object) {
+//         let { lyric } = object
+//         let array = lyric.split('\n')
+//         let reg = /\[(.+)\](.*)/
+//         let arr = []
+//         let $lyric = $('.lyric-moving')
+//         for (let i = 0; i < array.length; i++) {
+//             if (array[i]) {
+//                 arr.push(array[i].match(reg))
+//             }
+//             if(arr[i][2]){
+//             var $p = $('<p>')
+//             $p.attr('date-time', arr[i][1]).text(arr[i][2])
+//             $p.appendTo($lyric)
+//             }
+//         console.log(arr)
+//         }
+//     })
+// })
+$(function(){
+    $.get('./song.json').then(function(object){
+        let {lyric} = object
         let array = lyric.split('\n')
-        let reg = /\[(.+)\](.*)/
+        let reg = /\[(.*)\](.+)/
         let arr = []
         let $lyric = $('.lyric-moving')
-        for (let i = 0; i < array.length; i++) {
-            if (array[i]) {
-                arr.push(array[i].match(reg))
+        array = array.map((string) => {
+            let obj = {}
+            obj = string.match(reg)
+            return obj
+        })
+        for(var i=0;i<array.length;i++){
+            if(array[i]){
+                let $p = `
+                   <p data-time="${array[i][1]}">${array[i][2]}</p>
+                     `
+                $lyric.append($p)
             }
-            if(arr[i][2]){
-            var $p = $('<p>')
-            $p.attr('date-time', arr[i][1]).text(arr[i][2])
-            $p.appendTo($lyric)
-            }
-        console.log(arr)
         }
     })
 })
+setInterval(function(){
+     let time = audio.currentTime
+     let minute = ~~(time/60)
+     let newminute = (minute>9)?minute:'0'+minute
+     let second = time - minute*60
+     let newSecond = (second > 10)?second:`0`+second
+    // console.log(newSecond)
+     let newTime = `${newminute}:${newSecond}`
+     let $p = $('.lyric-moving>p')
+    $.each($p,function(item){
+        let pTime = $p.eq(item).attr('data-time')
+        let pNextTime = $p.eq(item+1).attr('data-time')
+        // console.log(pTime,pNextTime)
+        if(newTime >= pTime && newTime < pNextTime){
+            let movingPx = `-${(item-1)*24}px`
+            $('.lyric-moving').css('transform',`translateY(${movingPx})`)
+            $p.eq(item).css('color','white')
+            $p.eq(item-1).css('color','rgba(255,255,255,.6)')
+        }
+    })
+},300)
