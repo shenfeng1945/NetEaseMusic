c4e9afa2c4d63f16dd991d9a49eac8327f8cc948
shenfeng1945 <823902826@qq.com>
2017-08-29 20:23:39 +0800

    给页面加个loading,专辑页面基本完成了

--- special.js
+++ special.js
@@ -5,32 +5,61 @@ AV.init({
     appId: APP_ID,
     appKey: APP_KEY
 });
-var query = new AV.Query('Spec');
+var query = new AV.Query('Song');
 let url = window.location.href
 let reg = /.*\=(.*)/
 let number = url.match(reg)[1]
+let count = 0
+//默认全局隐藏，现loading
+// $('.page').addClass('hidden')
 query.find().then(function (results) {
-    console.log(typeof results)
-    for(let i=0;i<results.length;i++){
-       let array = results[i].attributes
-        console.log(array[i]['number'] === number)
-        if(array[i]['number'] === number){
-            getMessage({
-                fullname: array[i]['fullname'],
-                head: array[i]['head'],
-                summary: array[i]['summary']
-            })
+    console.log('1')
+    for(var i=0;i<results.length;i++){
+        let array = results[i].attributes
+        let id = results[i].id
+        let {name,singer,special} = array
+        if(array.number === number){
+            if(array.fullname){
+                getMessage({
+                    fullname: array.fullname,
+                    head: array.head,
+                    summary: array.summary
+                })
+            }else{
+                count ++
+                let li = `
+           <li>
+                <a href="./song.html?id=${id}">
+                    <div class="number">${count}</div>
+                    <div class="list-play-wrap">
+                        <div class="list-item">
+                            <h3>${name}</h3>
+                            <p>${singer} - ${special}</p>
+                        </div>
+                        <div class="play">
+                            <svg>
+                                <use xlink:href="#icon-play"></use>
+                            </svg>
+                        </div>
+                    </div>
+                </a>
+            </li> 
+                      `
+                $('.hot-songs > ul').append(li)
+                $('.hot-songs> h3').text(`热门单曲${count}首`)
+            }
         }
     }
+    $('.page').removeClass('hidden')
+    $('.loading').remove()
 })
 
     function getMessage(options){
-        let {fullName,head,summary} = options
+        let {fullname,head,summary} = options
         let $img = document.querySelector('.art-head > img')
         $img.src = head
-        $('.art-head > .name').text(fullName)
+        $('.art-head > .name').text(fullname)
         $('.summary > p').text(summary)
     }
-
 
 
